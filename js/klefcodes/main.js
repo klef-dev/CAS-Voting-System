53ca3156c34f4a4ec8c2e10e0fa187c880e4a3af
Abraham Ugbeshe <abrahamugbeshe@gmail.com>
2019-03-30 13:17:08 +0100

    Changed api url in main.js

--- js/klefcodes/main.js
+++ js/klefcodes/main.js
@@ -1,4 +1,4 @@
-const API = "http://cas-vs-server.herokuapp.com/public";
+const API = "https://cas-vs-server.herokuapp.com/public";
 const routes = [
   { path: "/", component: Home },
   { path: "*", component: NotFound },
